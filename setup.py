436142949d6d415e587443b805756ce80d02cdb3
Herminio Vazquez <herminio.vazquez@ing.com>
2020-03-31 04:12:16 +0200

    Pushing version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiowebhdfs", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Herminio Vazquez",
     author_email="canimus@gmail.com",
     description="A modern and asynchronous web client for WebHDFS",
